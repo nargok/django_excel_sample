d8dcd2d2a7cd74bf36764b2bf293b1e9858e1bfa
nargok <nargok@gmail.com>
2019-10-06 09:53:42 +0900

    upload excel file

---
+++ myapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+import openpyxl
+
+
+def index(request):
+    if 'GET' == request.method:
+        return render(request, 'myapp/index.html', {})
+    else:
+        excel_file = request.FILES['excel_file']
+
+        wb = openpyxl.load_workbook(excel_file)
+
+        worksheet = wb['Sheet1']
+        print(worksheet)
+
+        excel_data = list()
+        for row in worksheet.iter_rows():
+            row_data = list()
+            for cell in row:
+                row_data.append(str(cell.value))
+            excel_data.append(row_data)
+
+        return render(request, 'myapp/index.html', {'excel_data': excel_data})
